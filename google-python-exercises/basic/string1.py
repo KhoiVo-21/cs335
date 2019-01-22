91e4210e95a24e608706f06e902c5d489e9a9f28
EC2 Default User <ec2-user@ip-172-31-42-252.us-east-2.compute.internal>
2019-01-22 20:00:48 +0000

    strings

--- google-python-exercises/basic/string1.py
+++ google-python-exercises/basic/string1.py
@@ -24,16 +24,10 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-<<<<<<< HEAD
   if count < 10:
     return 'Number of donuts: %d' % (count)
   else:
     return 'Number of donuts: many' 
-=======
-  # +++your code here+++
-  return
-
->>>>>>> origin
 
 # B. both_ends
 # Given a string s, return a string made of the first 2
@@ -41,11 +35,7 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-<<<<<<< HEAD
-  #
-=======
   # +++your code here+++
->>>>>>> origin
   return
 
 
