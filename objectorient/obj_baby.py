283674ebe310f1b1bb540fef8dbb16fd85be9640
EC2 Default User <ec2-user@ip-172-31-42-252.us-east-2.compute.internal>
2019-02-12 16:40:28 +0000

    object baby names

--- objectorient/obj_baby.py
+++ objectorient/obj_baby.py
@@ -1,22 +1,48 @@
 #!/usr/bin/python -tt
 
+import sys
 import urllib
+import re
 
-def wget(url):
-    try:
-        text = urllib.urlopen(url)
-        if text.info().gettype() == "text/html":
-            print text.read()
-    except IOError as err:
-        print "could not access web address", url
-        print err
+
+class BabyNames:
+    def __init__(self, year):
+        self.year = year
         
-wget("http://www.bounty.com/pregnancy-and-birth/baby-names/top-baby-names/100-most-popular-boys-names-so-far-in-2018")
 
-def something():
-    text = ""
-    # pagetext = text.read()
-    pagetext = ""
-    beg = pagetext.search('<ol>')
-    end = pagetext.search('<ol>')
-    list = pagetext[beg:end]
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print 'usage: [--summaryfile] URL'
+        sys.exit(1)
+
+    summary = False
+    if args[0] == '--summaryfile':
+        summary = True
+        del args[0]
+        
+    try:
+        text = urllib.urlopen(args[0])
+        if text.info().gettype() == "text/html":
+            # years = re.findall(r'\d\d\d\d\sto\s\d\d\d\d</h2><ul>(.*?)</ul>', text.read())
+            years = re.findall(r'\d+\sto\s\d+</h2><ul>(.*?)</ul>', text.read())
+            for year in years:
+                print year
+    except IOError:
+        print "could not access web address", args[0]
+        
+        
+if __name__ == "__main__":
+    main()
+        
+        
+# wget("https://www.babycentre.co.uk/popular-baby-names")
+
+
+# def something():
+#     text = ""
+#     pagetext = text.read()
+#     pagetext = ""
+#     beg = pagetext.search('<ol>')
+#     end = pagetext.search('<ol>')
+#     list = pagetext[beg:end]
